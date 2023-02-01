a1cbfd006badd9d7a6fbd89463d06faab2e98bb9
Enzo Thomé <enzothome.et@gmail.com>
2023-02-01 01:33:50 -0300

    update: app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import Footer from './components/Footer';
 import Header from './components/Header';
+import Mysteries from './components/Mysteries';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <>
       <Header />
+      <Mysteries />
+      <Footer />
     </>
   );
 }
